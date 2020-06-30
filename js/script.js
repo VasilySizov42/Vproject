e79e5ea25e0309191e0ba5a11c1820cecb823894
VasilySizov42 <pinky-77@yandex.ru>
2020-06-30 14:23:12 +0300

    Practice 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMouvieDB = {
     count: numberOfFilms,
@@ -6,11 +8,52 @@ const personalMouvieDB = {
     genres: [],
     privat: false
 };
-const firstFilm = prompt('Один из последних просмотренных фильмов?', ''),
-      firstRate = +prompt('На сколько оцените его?', ''),
-      secFilm = prompt('Один из последних просмотренных фильмов?', '');
-      secRate = +prompt('На сколько оцените его?', '');
-personalMouvieDB.muvies[firstFilm] = firstRate;
-personalMouvieDB.muvies[secFilm] = secRate;
 
+/* for (let i = 0; i< 2; i++) {
+    const film = prompt('Один из последних просмотренных фильмов?', ''),
+          rate = +prompt('На сколько оцените его?', '');
+    if (film != null && rate != null && film != '' && rate != '' && film.length <= 50){
+        personalMouvieDB.muvies[film] = rate;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }    
+} */
+/* let i = 0;
+while (i<2){
+    const film = prompt('Один из последних просмотренных фильмов?', ''),
+          rate = +prompt('На сколько оцените его?', '');
+    if (film != null && rate != null && film != '' && rate != '' && film.length <= 50){
+        personalMouvieDB.muvies[film] = rate;
+        console.log('done');
+        i++;
+    } else {
+        console.log('error');
+        continue;
+    }    
+} */
+let i = 0;
+do{
+    const film = prompt('Один из последних просмотренных фильмов?', ''),
+          rate = +prompt('На сколько оцените его?', '');
+    if (film != null && rate != null && film != '' && rate != '' && film.length <= 50){
+        personalMouvieDB.muvies[film] = rate;
+        console.log('done');
+        i++;
+    } else {
+        console.log('error');
+        continue;
+    }    
+} while (i<2)
 console.log(personalMouvieDB);
+
+if (personalMouvieDB.count < 10){
+    console.log('Посмотрено довольно мало фильмов');
+} else if(personalMouvieDB.count >= 10 && personalMouvieDB.count <30){
+    console.log('Вы классический зритель');
+} else if(personalMouvieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
